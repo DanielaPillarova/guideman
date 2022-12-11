3c46e79a7dc378a217937566bb7c1b0ea500dc68
danielapillarova <danielapillarova@localhost>
2022-12-11 21:23:19 +0100

    all windows doneee

---
+++ src/main/java/sk/upjs/paz1c/guideman/controllers/CreateTourController.java
@@ -0,0 +1,110 @@
+package sk.upjs.paz1c.guideman.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CreateTourController {
+
+    @FXML
+    private TextArea bioTextArea;
+
+    @FXML
+    private ComboBox<?> chooseTourComboBox;
+
+    @FXML
+    private TextField cityTextField;
+
+    @FXML
+    private TextField countryTextField;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button createTourButton;
+
+    @FXML
+    private TextField dateAndTimeOfTourTextField;
+
+    @FXML
+    private TextField durationTextField;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Button myProfileButton;
+
+    @FXML
+    private Button myToursButton;
+
+    @FXML
+    private Label noSelectedImageLabel;
+
+    @FXML
+    private TextField numberOfPeopleTextField;
+
+    @FXML
+    private TextField priceTextField;
+
+    @FXML
+    private Button searchTourButton;
+
+    @FXML
+    private Button selectImageButton;
+
+    @FXML
+    private TextField streetNumberTextField;
+
+    @FXML
+    private TextField streetTextField;
+
+    @FXML
+    private TextField titleTextField;
+
+    @FXML
+    void createButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createTourButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOutButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void myProfileButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void myToursButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchTourButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectImageButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+       
+    }
+
+}
---
+++ src/main/java/sk/upjs/paz1c/guideman/controllers/SearchTourController.java
@@ -0,0 +1,87 @@
+package sk.upjs.paz1c.guideman.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class SearchTourController {
+
+    @FXML
+    private Label countryLabel;
+
+    @FXML
+    private Button createTourButton;
+
+    @FXML
+    private ListView<?> filteredToursListView;
+
+    @FXML
+    private Label guidemanLabel;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Label monthLabel;
+
+    @FXML
+    private Button myProfileButton;
+
+    @FXML
+    private Button myToursButton;
+
+    @FXML
+    private Label priceLabel;
+
+    @FXML
+    private Button searchTourButton;
+
+    @FXML
+    private Button showFilterTableButton;
+
+    @FXML
+    private Button showTourButton;
+
+    @FXML
+    void createTourButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOutButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void myProfileButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void myToursButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchTourButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showFilterTableButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showTourButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+       
+    }
+
+}
