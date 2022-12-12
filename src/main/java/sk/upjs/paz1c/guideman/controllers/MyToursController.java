c9fb1ad00fccf23f074de2f8bb436f7fcdb95bd8
danielapillarova <danielapillarova@localhost>
2022-12-12 15:39:37 +0100

    started my tours

--- src/main/java/sk/upjs/paz1c/guideman/controllers/MyToursController.java
+++ src/main/java/sk/upjs/paz1c/guideman/controllers/MyToursController.java
@@ -1,98 +1,183 @@
 package sk.upjs.paz1c.guideman.controllers;
 
-import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.control.ListView;
+import sk.upjs.paz1c.guideman.storage.DaoFactory;
+import sk.upjs.paz1c.guideman.storage.Event;
+import sk.upjs.paz1c.guideman.storage.EventDao;
+import sk.upjs.paz1c.guideman.storage.Tour;
+import sk.upjs.paz1c.guideman.storage.TourDao;
 
 public class MyToursController {
 
-    @FXML
-    private Button addRatingOrReviewButton;
+	private TourDao tourDao;
+	private EventDao eventDao;
+	private Long loggedUserId;
+	
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
 
-    @FXML
-    private Button createTourButton;
+	@FXML
+	private Button addRatingOrReviewButton;
 
-    @FXML
-    private Button deleteTourButton;
+	@FXML
+	private Button createTourButton;
 
-    @FXML
-    private CheckBox futureToursCheckBox;
+	@FXML
+	private Button deleteTourButton;
 
-    @FXML
-    private Button logOutButton;
+	@FXML
+	private CheckBox futureToursCheckBox;
 
-    @FXML
-    private Button myProfileButton;
+	@FXML
+	private Button logOutButton;
 
-    @FXML
-    private Button myToursButton;
+	@FXML
+	private Button myProfileButton;
 
-    @FXML
-    private Label myToursLabel;
+	@FXML
+	private Button myToursButton;
 
-    @FXML
-    private CheckBox pastToursCheckBox;
+	@FXML
+	private Label myToursLabel;
 
-    @FXML
-    private Button searchTourButton;
+	@FXML
+	private CheckBox pastToursCheckBox;
 
-    @FXML
-    private Button showTourButton;
+	@FXML
+	private Button searchTourButton;
 
-    @FXML
-    private CheckBox toursWhereIAmGuidemanCheckBox;
+	@FXML
+	private Button showTourButton;
 
-    @FXML
-    void addRatingOrReviewButtonAction(ActionEvent event) {
+	@FXML
+	private ListView<String> toursListView;
 
-    }
-    
-    @FXML
-    void deleteTourButtonAction(ActionEvent event) {
+	@FXML
+	private CheckBox toursWhereIAmGuidemanCheckBox;
 
-    }
-    
-    @FXML
-    void showTourButtonAction(ActionEvent event) {
+	
+	
+	@FXML
+	void addRatingOrReviewButtonAction(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void myProfileButtonAction(ActionEvent event) {
+	@FXML
+	void deleteTourButtonAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	void showTourButtonAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	void myProfileButtonAction(ActionEvent event) {
 		Menu.INSTANCE.openMyProfile(addRatingOrReviewButton);
-    }
+	}
 
-    @FXML
-    void myToursButtonAction(ActionEvent event) {
-    	System.out.println("tours");
-    }
+	@FXML
+	void myToursButtonAction(ActionEvent event) {
+		System.out.println("tours");
+	}
 
-    @FXML
-    void searchTourButtonAction(ActionEvent event) {
-    	Menu.INSTANCE.openSearchTour(addRatingOrReviewButton);
-    }
-    
-    @FXML
-    void createTourButtonAction(ActionEvent event) {
-    	Menu.INSTANCE.openCreateTour(addRatingOrReviewButton);
-    }
-    
-    @FXML
-    void logOutButtonAction(ActionEvent event) {
-    	Menu.INSTANCE.logOut(addRatingOrReviewButton);
-    }
-   
+	@FXML
+	void searchTourButtonAction(ActionEvent event) {
+		Menu.INSTANCE.openSearchTour(addRatingOrReviewButton);
+	}
 
-    @FXML
-    void initialize() {
-        
+	@FXML
+	void createTourButtonAction(ActionEvent event) {
+		Menu.INSTANCE.openCreateTour(addRatingOrReviewButton);
+	}
+
+	@FXML
+	void logOutButtonAction(ActionEvent event) {
+		Menu.INSTANCE.logOut(addRatingOrReviewButton);
+	}
+
+	private void showToursAtBeginning() {	
+		List<String> allToursInListView = new ArrayList<>();
+		List<Tour> allTours = tourDao.getAll();
+		for (Tour t : allTours) {
+			List<Event> allEventsByTourId = eventDao.getAllByTour(t.getId());
+			for (Event e : allEventsByTourId ) {
+				allToursInListView.add(parseDateAndTime(t, e, e.getDateOfTour()));
+			}
+		}
+		toursListView.setItems(FXCollections.observableArrayList(allToursInListView));
+	}
+	
+	private String parseDateAndTime(Tour t, Event e, LocalDateTime localDateTime) {
+		String[] dt = localDateTime.toString().split("T");
+		String[] date = dt[0].split("-");
+		String time = dt[1];
+		StringBuilder sbDate = new StringBuilder();
+		sbDate.append(date[2]);
+		sbDate.append(".");
+		sbDate.append(date[1]);
+		sbDate.append(".");
+		sbDate.append(date[0]);
+		return "Title : " + t.getTitle() + ",           date of tour : " 
+		+ sbDate + ",          time of tour : " + time + ",          price : " + e.getPrice();
+	}
+	
+	@FXML
+	void pastToursChecked(ActionEvent event) {
+		if (pastToursCheckBox.isSelected()) {
+			System.out.println("selecteeeeeddd");
+			List<String> allToursInListView = new ArrayList<>();
+			List<Tour> tours = tourDao.getAllToursFromPast(loggedUserId);
+			List<Event> events = eventDao.getAllEventsFromPast(loggedUserId);
+			System.out.println(tours.toString());
+			System.out.println(events.toString());
+			int idx = 0;
+			while (idx < events.size()) {
+				String s = parseDateAndTime(tours.get(idx), events.get(idx), events.get(idx).getDateOfTour());
+				allToursInListView.add(s);
+				System.out.println(s);
+				idx++;
+			}
+			toursListView.setItems(FXCollections.observableArrayList(allToursInListView));
+			
+			// TODO ked nema ziadne past
+		}
+	}
+
+	@FXML
+    void futureToursChecked(ActionEvent event) {
+
     }
+	
+	@FXML
+    void toursWhereIAmGuidemanChecked(ActionEvent event) {
+
+    }
+	
+//	private void metoda(List<Tour> tours, List<Event> events) {
+//		
+//	}
+
+
+	@FXML
+	void initialize() {
+		tourDao = DaoFactory.INSTANCE.getTourDao();
+		eventDao = DaoFactory.INSTANCE.getEventDao();
+		loggedUserId = LoggedUser.INSTANCE.getLoggedUser().getId();
+		showToursAtBeginning();
+
+	}
 
 }
--- src/main/java/sk/upjs/paz1c/guideman/models/UserFxModel.java
+++ src/main/java/sk/upjs/paz1c/guideman/models/UserFxModel.java
@@ -2,15 +2,19 @@ package sk.upjs.paz1c.guideman.models;
 
 import java.sql.Blob;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import javax.swing.text.DateFormatter;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import sk.upjs.paz1c.guideman.storage.User;
 
-// idk ci je to dobre
-// najskor nie, nemam tusenia co tu robit
 
 public class UserFxModel {
 
@@ -19,12 +23,14 @@ public class UserFxModel {
 	private StringProperty surname = new SimpleStringProperty();
 	private StringProperty email = new SimpleStringProperty();
 	private StringProperty telNumber = new SimpleStringProperty();
-	private LocalDate birthdate;
+	private ObjectProperty<LocalDateTime> birthdate = new SimpleObjectProperty<>();
 	private StringProperty login = new SimpleStringProperty();
 	private StringProperty password = new SimpleStringProperty();
 	private Blob image;
 
 	private ObservableList<User> users;
+	
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
 	public UserFxModel() {
 		users = FXCollections.observableArrayList();
@@ -38,7 +44,7 @@ public class UserFxModel {
 		if (user.getTelNumber() != null) {
 			telNumber.set(user.getTelNumber());
 		}
-		this.birthdate = user.getBirthdate();
+		birthdate.set(LocalDateTime.parse(user.getBirthdate().toString(), formatter));
 		login.set(user.getLogin());
 		password.set(user.getPassword());
 		if (user.getImage() != null) {
@@ -50,9 +56,42 @@ public class UserFxModel {
 	public ObservableList<User> getUsersModel() {
 		return users;
 	}
+	
 
 	public StringProperty nameProperty() {
 		return name;
 	}
+	
+	public StringProperty surnameProperty() {
+		return surname;
+	}
+	
+	public StringProperty emailProperty() {
+		return email;
+	}
+	
+	public StringProperty telNumberProperty() {
+		return name;
+	}
+	
+	public ObjectProperty<LocalDateTime> birthdateProperty() {
+		return birthdate;
+	}
+	
+	public LocalDate getBirthdate() {
+		return birthdate.get().toLocalDate();
+	}
+	
+	public void setBirthdate(LocalDate localDate) {
+		this.birthdate.set(LocalDateTime.parse(localDate.toString(), formatter));
+	}
 
+	public StringProperty loginProperty() {
+		return login;
+	}
+	
+	public StringProperty passwordProperty() {
+		return password;
+	}
+	
 }
